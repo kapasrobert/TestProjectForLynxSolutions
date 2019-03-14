a8099cc6e15bc7f3727dfefc4d3e290257bd17de
Robert Kapas <kapas.robert.j@gmail.com>
2019-03-14 17:56:56 +0200

    Add project files.

---
+++ TestProjectForLynxSolutions/App_Start/FilterConfig.cs
@@ -0,0 +1,13 @@
+﻿using System.Web;
+using System.Web.Mvc;
+
+namespace TestProjectForLynxSolutions
+{
+    public class FilterConfig
+    {
+        public static void RegisterGlobalFilters(GlobalFilterCollection filters)
+        {
+            filters.Add(new HandleErrorAttribute());
+        }
+    }
+}
