acf3a1e09e968a85d01ab1f8e15586e457b5bc49
Robert Kapas <kapas.robert.j@gmail.com>
2019-03-14 21:42:24 +0200

    Added the Edit user functionality

--- TestProjectForLynxSolutions/Controllers/UsersController.cs
+++ TestProjectForLynxSolutions/Controllers/UsersController.cs
@@ -9,16 +9,19 @@ namespace TestProjectForLynxSolutions.Controllers
     {
         private MyDbContext myDbContext;
 
+
         public UsersController()
         {
             myDbContext = new MyDbContext();
         }
+
 
         protected override void Dispose(bool disposing)
         {
             base.Dispose(disposing);
             myDbContext.Dispose();
         }
+
 
         //Display all users
         public ViewResult Index()
@@ -27,6 +30,7 @@ namespace TestProjectForLynxSolutions.Controllers
 
             return View(users);
         }
+
 
         //User detail
         public ActionResult Details(int id)
@@ -46,6 +50,7 @@ namespace TestProjectForLynxSolutions.Controllers
             return View();
         }
 
+
         //Getting new user from view, and saving it into database
         [HttpPost]
         public ActionResult Create(User user)
@@ -56,6 +61,44 @@ namespace TestProjectForLynxSolutions.Controllers
                 myDbContext.SaveChanges();
             }
             catch(Exception ex)
+            {                
+                //error message
+            }
+
+            return RedirectToAction("Index", "Users");
+        }
+
+
+        //Get a user and load the edit page for that user
+        public ActionResult Edit(int id)
+        {
+            var user = myDbContext.Users.SingleOrDefault(u => u.Id == id);
+
+            if (user == null)
+                return HttpNotFound();
+
+            return View(user);
+        }
+
+
+        //save the edited user to the database
+        [HttpPost]
+        public ActionResult Save(User user)
+        {
+            try
+            {
+                var userInDatabase = myDbContext.Users.Single(u => u.Id == user.Id);
+
+                userInDatabase.UserName = user.UserName;
+                userInDatabase.Password = user.Password;
+                userInDatabase.FirstName = user.FirstName;
+                userInDatabase.LastName = user.LastName;
+                userInDatabase.Email = user.Email;
+                userInDatabase.PhoneNumber = user.PhoneNumber;
+
+                myDbContext.SaveChanges();
+            }
+            catch (Exception ex)
             {
                 //error message
             }
